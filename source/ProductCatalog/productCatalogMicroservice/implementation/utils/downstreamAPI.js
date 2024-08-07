37ca05c3eaa9ef528ddce7b5810e5de84298cfba
Lester <lesterthomas@hotmail.com>
2024-08-07 16:51:44 +0100

    Added standalone canvas info service for testing

--- source/ProductCatalog/productCatalogMicroservice/implementation/utils/downstreamAPI.js
+++ source/ProductCatalog/productCatalogMicroservice/implementation/utils/downstreamAPI.js
@@ -1,8 +1,42 @@
 'use strict';
 const axios = require('axios');
 
-const downstreamAPIList = ['http://localhost/r1-productcatalogmanagement/tmf-api/productCatalogManagement/v4/']
+const CANVAS_INFO_SERVICE_INVENTORY_API = 'http://localhost:8638/tmf-api/serviceInventoryManagement/v5/' // 'http://info.canvas.svc.cluster.local/tmf-api/serviceInventoryManagement/v5/'
+/**
+ * This function retrieves a list of downstream APIs that the product catalog microservice is dependent on.
+ * This function calls the Canvas.Info Service Inventory API at info.canvas.svc.cluster.local to get the list
+ * of downstream APIs to call. The Service Inventory should return a list of zero or more services or type API that
+ * have a Service Characteristic of 'url'. 
+ * @returns The list of downstream APIs
+ */
+async function getDownstreamAPIs() {
+    console.log('utils/downstreamAPI/getDownstreamAPIs :: getting list of downstream APIs from ' + CANVAS_INFO_SERVICE_INVENTORY_API + 'service');
+    try {
+        const apiResponse = await axios.get(CANVAS_INFO_SERVICE_INVENTORY_API + 'service')
+        if (apiResponse.data) {
+            console.log('utils/downstreamAPI/getDownstreamAPIs :: received ' + apiResponse.data.length + ' records');
+            // look for services with a service characteristic of 'url'
+            const downstreamAPIList = [];
+            // for each service in the apiResponse.data, check if it has a serviceCharacteristic of 'url'
+            for (const service in apiResponse.data) {
+                if (apiResponse.data[service].serviceCharacteristic) {
+                    for (const serviceCharacteristic in apiResponse.data[service].serviceCharacteristic) {
+                        if (apiResponse.data[service].serviceCharacteristic[serviceCharacteristic].name === 'url') {
+                            downstreamAPIList.push(apiResponse.data[service].serviceCharacteristic[serviceCharacteristic].value);
+                        }
+                    }
+                }
+            }
+            console.log('utils/downstreamAPI/getDownstreamAPIs :: returning ' + downstreamAPIList.length + ' downstream APIs');
 
+            return downstreamAPIList;
+        }
+    }
+    catch (AxiosError) {
+        console.log('utils/downstreamAPI/retrieveFromDownstreamAPI :: error getting data from downstream API at ' + CANVAS_INFO_SERVICE_INVENTORY_API);
+        console.log(AxiosError);
+    }
+}
 
 /**
  * This queries a list of downstream Product Catalog APIs and concatenates the results.
@@ -16,6 +50,7 @@ const downstreamAPIList = ['http://localhost/r1-productcatalogmanagement/tmf-api
 async function listFromDownstreamAPI(doc, url) {
     console.log(url);
     const urlResource = url.split('/').pop();
+    const downstreamAPIList = await getDownstreamAPIs();
     for (const downstreamAPI in downstreamAPIList) {
         console.log('utils/downstreamAPI/listFromDownstreamAPI :: getting data from downstream API at ' + downstreamAPIList[downstreamAPI] + urlResource);
         const apiResponse = await axios.get(downstreamAPIList[downstreamAPI] + urlResource)
@@ -27,6 +62,7 @@ async function listFromDownstreamAPI(doc, url) {
 
 async function retrieveFromDownstreamAPI(resourceType, id) {
     console.log('utils/downstreamAPI/retrieveFromDownstreamAPI :: getting data from downstream API for resource ' + resourceType + ' and id ' + id);
+    const downstreamAPIList = await getDownstreamAPIs();
     for (const downstreamAPI in downstreamAPIList) {
         console.log('utils/downstreamAPI/retrieveFromDownstreamAPI :: getting data from downstream API at ' + downstreamAPIList[downstreamAPI] + resourceType + '/' + id);
         try {
--- source/ProductCatalog/tests/README.md
+++ source/ProductCatalog/tests/README.md
@@ -1,10 +1,19 @@
 # Postman collection to test the Product Catalog Component
 
-Here is a sample postman collection to test the Product Catalog Component - you need to create an environment declaring the {{Hostname}} - get this from the API_ENDPOINT on the r1-productcatalog-productcatalogmanagement API resource - just use the first part of the URL with the hostname.
+Here is a sample postman collection to test the Product Catalog Component.
+These can be used to test the code as a standalone software running on the command-line, or as a deployed component running in an ODA Canvas. You need to create an environment in Postman declaring the {{Hostname}}. 
+* For testing standalone, this will typically be `localhost:8080`.
+* For testing in an ODA Canvas you need to query the API_ENDPOINT on the r1-productcatalog-productcatalogmanagement API resource - just use the first part of the URL with the hostname.
 
 If you have applied API Keys or other authentication tokens in your deployment, you will need to add these in the headders in the postman collection.
 
-There are three test colections:
+There are three test colections for the Product Catalog Management:
 * Functional test of the Product Catalog API
 * Bulk deletion from the Product Catalog API
 * Testing the role and event listeners
+
+And one test collection for the Promotion Management.
+
+
+
+For standalone testing of the Canvas.Info service, there is a standalone Nodejs implementation of the Service Inventory Open API. To execute this as standalone, you need to create a local `hosts` file with a `info.canvas.svc.cluster.local` url pointing to the Nodejs implementation.
